543ef53f3d80ea9c21721e46a410b980e4844b37
Sebastien <sebdevil2@gmail.com>
2019-03-28 12:10:21 -0600

    CellComponent has a dropdown

--- src/client/app/brackets/cell/cell.component.ts
+++ src/client/app/brackets/cell/cell.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnInit } from '@angular/core';
 import { Team } from '../../shared/team';
 
 @Component({
@@ -8,10 +8,12 @@ import { Team } from '../../shared/team';
   styleUrls: ['./cell.component.css']
 })
 
-export class CellComponent implements OnChanges{
+export class CellComponent implements OnChanges, OnInit{
   @Input() team: Team;
+  @Input() teamChoices: Team[];
   @Input() class: string;
   position: string;
+  selectedValue: Team = null;
 
   @Output() cellClicked: EventEmitter<string> = new EventEmitter<string>();
 
@@ -25,5 +27,14 @@ export class CellComponent implements OnChanges{
   }
   onClick(): void {
     this.cellClicked.emit(`The team ${this.team.TeamName} was clicked!`);
+  }
+
+  ngOnInit(): void {
+    this.selectedValue = this.teamChoices[this.team.Seed - 1];
+    this.position = `Team ${this.team.Seed}`;
+  }
+
+  compareFn(t1: Team, t2: Team): boolean {
+    return t1 && t2 ? t1.TeamName === t2.TeamName : t1 === t2;
   }
 }
